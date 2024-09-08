ed70e290af9562ef4116e39a2478a893baf923fb
Vlad <example.com>
2024-09-08 17:29:08 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+console.log(10 + +'10');
+
+let incr = 15,
+    decr = 15;
+  
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || !isClose);
